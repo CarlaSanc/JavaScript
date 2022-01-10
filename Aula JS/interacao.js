161523c05e4f41f716a57bdc31b01383386ab4eb
CarlaSanc <90981239+CarlaSanc@users.noreply.github.com>
2022-01-10 17:57:16 -0300

    Initial commit

---
+++ Aula JS/interacao.js
@@ -0,0 +1,16 @@
+
+
+
+//comentários em JavaScript pode ser /** */
+// booleanos guardas os valores true ou false
+
+
+let nome = prompt("Qual o seu nome?") 
+
+//pergunta o nome dentro da caixa -->alert("É um grande prazer em te conhecer, " + nome + "!")
+
+const ano_nascimento = parseInt(prompt("Digite seu ano de nascimento")) 
+let anoatual = 2021
+let idade =  2021 - ano_nascimento  
+alert("Olá, seu nome é  " + nome + ". Você tem "  +   idade +   "  anos.")
+//Olá, seu nome é... e você tem ... anos
